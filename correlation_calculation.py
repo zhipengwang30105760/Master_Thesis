df5d16f2b7496bcc122031397a9d61161b0fb819
Zhipeng Wang <50890422+ziipeen@users.noreply.github.com>
2020-08-17 08:09:54 -0500

    add combination function

--- correlation_calculation.py
+++ correlation_calculation.py
@@ -1,18 +1,18 @@
+from itertools import combinations
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import statsmodels.api as sm
-from sklearn.metrics import confusion_matrix
-from sklearn.model_selection import train_test_split
-from sklearn.svm import SVC
+
 
 from main_program import GENERATE_CONFUSION_MATRIX
 
 
 
 #find the correlation value and drop the value which is larger than 0.34?
-def correlation_heatmap(train):
+def correlation_heatmap(train, upper_bound, lower_bound):
     correlations = train.corr()
     fig, ax = plt.subplots(figsize=(20, 20))
     sns.heatmap(correlations, vmax=1.0, center=0, fmt='.2f',
@@ -21,9 +21,12 @@ def correlation_heatmap(train):
     columns = np.full((correlations.shape[0],), True, dtype=bool)
     for i in range(correlations.shape[0]):
         for j in range(i+1, correlations.shape[0]):
-            if correlations.iloc[i,j] >=0.9:
+            if correlations.iloc[i,j] >= upper_bound or correlations.iloc[i,j] <= lower_bound:
                 if columns[j]:
                     columns[j] = False;
+                #drop both feature
+                # if(columns[i]):
+                #     columns[i] = False;
 
     selected_columns = train.columns[columns]
     #print(selected_columns[0:].values)
@@ -53,28 +56,59 @@ def backwardElimination(x, Y, sl, columns):
 
 
 if __name__ == "__main__":
-    filename = r"/Users/zhipengwang/PycharmProjects/UNMC_Data_Analysis/data/oversampling_Readmission_1.csv"
+    filename = r"/Users/zhipengwang/PycharmProjects/UNMC_Data_Analysis/data/original_Readmission_1.csv"
     target="Readmission_1"
+    noisy_features = ['AGE', 'SMOKE', 'FNSTATUS2', 'HXCOPD', 'DIALYSIS', 'TRANSFUS', 'radial_all_yn', 'PRSEPIS']
     origin_data = pd.read_csv(filename)
-    data, selected_columns = correlation_heatmap(origin_data)
+    data, selected_columns = correlation_heatmap(origin_data, upper_bound= 0.25, lower_bound= -0.2)
 
     selected_columns = selected_columns[1:].values
 
-    SL = 0.05
+    #decide whether use p-value verification or not
+    #SL = 0.05
+    # data_modeled, selected_columns = backwardElimination(data.iloc[:, 1:].values, data.iloc[:, 0].values, SL, selected_columns)
+    #
+    # result = pd.DataFrame()
+    # result['diagnosis'] = data.iloc[:, 0]
+    # data = pd.DataFrame(data=data_modeled, columns=selected_columns)
+    selected_columns = list(selected_columns)
+    for features in selected_columns:
+        if features in noisy_features or features == target:
+            selected_columns.remove(features)
 
-    data_modeled, selected_columns = backwardElimination(data.iloc[:, 1:].values, data.iloc[:, 0].values, SL, selected_columns)
-
-    result = pd.DataFrame()
-    result['diagnosis'] = data.iloc[:, 0]
-    data = pd.DataFrame(data=data_modeled, columns=selected_columns)
-
-    print(selected_columns)
     #X = origin_data.drop([target], axis=1)
-    X = data
-    if target in selected_columns:
-        X = X.drop([target], axis=1)
+    #X = data
+
+    X= origin_data.loc[:, selected_columns]
 
     y = origin_data[target]
     r = GENERATE_CONFUSION_MATRIX(X, y, 0.2, target)
-    print(r)
+    score = r[0][0] + r[0][1] + r[1][0] + r[1][1]
+    score = (r[0][0] + r[1][1]) / score
 
+    print('Original result for selected correlated features without permutation')
+    print(selected_columns)
+    print(r)
+    print(score)
+
+
+    print('============================Get permutation list========================')
+    comb = combinations(selected_columns, 10)
+    print('============================Start Calculating===========================')
+    i=1
+    # print('number of all combination ' + str(sum( 1 for i in comb)))
+    for combination in comb:
+        print('========================Now at ' + str(i) + '==================')
+        i += 1
+        X = origin_data.loc[:, list(combination)]
+        y = origin_data[target]
+        curr_r = GENERATE_CONFUSION_MATRIX(X, y, 0.2, target)
+        curr_score = curr_r[0][0] + curr_r[0][1] + curr_r[1][0] + curr_r[1][1]
+        curr_score = (curr_r[0][0] + curr_r[1][1]) / curr_score
+        if curr_score > score:
+            print('============================Find Good Score===========================')
+            print(list(combination))
+            print(curr_r)
+            print(curr_score)
+            print('============================Keep Going===========================')
+    print('==============================Finished===========================')
